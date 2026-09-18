044a82a5cec5cf3438f5d68b725b7d39caac30f7
aleksandre ramishvili <ramishvilialeksandre78@gmail.com>
2026-09-18 18:48:43 +0400

    aritmetical operators

---
+++ script.js
@@ -0,0 +1,15 @@
+let username = "Aleksandre";
+
+const userEmail = "Ramishvilialeksandre78@gmail.com";
+
+let phoneNumber = 511224225;
+
+let age = 10;
+
+let profilePicture = true;
+
+let isStudent = false;
+
+let adress = "cabadze 15";
+
+console.log(age);
